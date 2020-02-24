76489e88ef9c6a8e20fd8665bdd3798c87a7dab2
guilarf <guilherme.ferreira@buildit.com.br>
2020-02-24 11:40:28 -0300

    Internacionalizacao e Script/Estilos Bundle -- Carregar varios arquivos juntos

--- LojaRazor/App_Start/BundleConfig.cs
+++ LojaRazor/App_Start/BundleConfig.cs
@@ -8,7 +8,16 @@ namespace LojaRazor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            StyleBundle estilo = new StyleBundle("~/bundles/estilos");
+            estilo.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilo.Include("~/Content/Style.css");
+            bundles.Add(estilo);
+
+            ScriptBundle scripts = new ScriptBundle("~/bundles/scripts");
+            scripts.Include("~/Scripts/jquery-1.7.1.js");
+            scripts.Include("~/Scripts/jquery.validade.js");
+            scripts.Include("~/Scripts/jquery.validade.unobtrusive.js");
+            bundles.Add(scripts);
         }
     }
 }
