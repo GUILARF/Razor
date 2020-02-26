bcaa2fbe1813de4ad61102e2c732c75aafaac124
guilarf <guilherme.ferreira@buildit.com.br>
2020-02-26 11:23:28 -0300

    NHibernate

--- TestaConexao/Models/Usuario.cs
+++ TestaConexao/Models/Usuario.cs
@@ -1,38 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace LojaRazor.Models
+namespace TestaConexao.Models
 {
     public class Usuario
     {
         public virtual int Id { get; set; }
 
-        [Required]
+        
         public virtual String Nome { get; set; }
 
-        [Required]
+        
         public virtual Sexo Sexo { get; set; }
 
-        [Required, EmailAddress]
+        
         public virtual String Email { get; set; }
 
-        [Required, MinLength(3)]
+        
         public virtual String Senha { get; set; }
 
-        [Required]
+        
         public virtual DateTime DataDeNascimento { get; set; }
 
-        [Required]
+        
         public virtual String Endereco { get; set; }
 
         public virtual Estado Estado { get; set; }
 
         public virtual String Complemento { get; set; }
 
-        [Required, RegularExpression("\\d{5}-\\d{3}")]
+        
         public virtual String CEP { get; set; }
 
         public virtual string Observacoes { get; set; }
---
+++ TestaConexao/Program.cs
@@ -0,0 +1,18 @@
+﻿using TestaConexao.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaConexao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NHibernateHelper.GeraSchema();
+            Console.ReadLine();
+        }
+    }
+}
