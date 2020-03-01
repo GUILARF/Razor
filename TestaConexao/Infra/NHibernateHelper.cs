b59ee8b6237f23be6ab7ddce9ee16830e29eeac1
guilarf <guilherme.ferreira@buildit.com.br>
2020-02-29 21:40:20 -0300

    Novo projeto: Injecao de dependencias na Session

--- TestaConexao/Infra/NHibernateHelper.cs
+++ TestaConexao/Infra/NHibernateHelper.cs
@@ -1,4 +1,5 @@
-﻿using NHibernate.Cfg;
+﻿using NHibernate;
+using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,13 @@ namespace TestaConexao.Infra
 {
     public class NHibernateHelper
     {
+        private static ISessionFactory fabrica = CriaSessionFactory();
+        private static ISessionFactory CriaSessionFactory()
+        {
+            Configuration cfg = RecuperarConfiguracao();
+            return cfg.BuildSessionFactory();
+        }
+
         public static Configuration RecuperarConfiguracao()
         {
             Configuration cfg = new Configuration();
@@ -22,5 +30,12 @@ namespace TestaConexao.Infra
             Configuration cfg = RecuperarConfiguracao();
             new SchemaExport(cfg).Create(true, true);
         }
+
+
+        public static ISession AbreSession()
+        {
+            return fabrica.OpenSession();
+        }
+
     }
 }
--- TestaConexao/Program.cs
+++ TestaConexao/Program.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate;
+using TestaConexao.Models;
+using TestaConexao.DAO;
 
 namespace TestaConexao
 {
@@ -11,8 +15,47 @@ namespace TestaConexao
     {
         static void Main(string[] args)
         {
-            NHibernateHelper.GeraSchema();
-            Console.ReadLine();
+            //NHibernateHelper.GeraSchema();
+            //Console.ReadLine();
+
+            //Configuration cfg = NHibernateHelper.RecuperarConfiguracao();
+            //ISessionFactory sessionFactory = cfg.BuildSessionFactory();
+            //ISession session = sessionFactory.OpenSession();
+            Usuario novoUsuario = new Usuario();
+            novoUsuario.Nome = "Guilherme";
+
+            ISession session = NHibernateHelper.AbreSession();
+            UsuariosDAO usuariosDAO = new UsuariosDAO(session);
+            usuariosDAO.Adiciona(novoUsuario);
+
+            ITransaction transacao = session.BeginTransaction();
+            session.Save(novoUsuario);
+            transacao.Commit(); 
+            session.Close();
+
+
+
+            //ITransaction Transacao = session.BeginTransaction();
+            // Nesse ponto o Usuario tem a propriedade nome com o valor Guilherme.
+            Usuario UsuarioDoBanco = session.Get<Usuario>(1);
+
+            UsuarioDoBanco.Nome = "Guilherme R. Ferreira";
+            Console.WriteLine("No commit, o NHibernate detecta que o Usuario foi modificado e " +
+                                                            "executa um Update no banco de dados");
+            transacao.Commit();
+
+
+
+            //ITransaction Transacao = session.BeginTransaction();
+            Usuario usuario = session.Get<Usuario>(1);
+            session.Delete(usuario);
+            transacao.Commit();
+
+            Usuario usuarioantigo = session.Get<Usuario>(1);
+            session.Close();
+
+            Console.Read();
+
         }
     }
 }
